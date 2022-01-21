9f9e9d4054afea7e6dab024dcf3a9f5dff1ed01e
slobbie <haeru9410@gmail.com>
2022-01-21 11:14:51 +0900

    todolist web 초기세팅

---
+++ todolist/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import './styles/init.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <TodoTemplate>안녕하세요</TodoTemplate>
+    </div>
+  );
+}
+
+export default App;
---
+++ todolist/src/components/TodoTemplate.tsx
@@ -0,0 +1,5 @@
+const TodoTemplate = ({ children }: any) => {
+  return <div className='TodoTemplateBlock'>{children}</div>;
+};
+
+export default TodoTemplate;
